c2014fa221125457ba628fb6847cd9cb41c1e40e
Mansor Almossa <malmossa@gmail.com>
2022-04-20 22:44:37 -0700

    Completed

--- server/server.js
+++ server/server.js
@@ -4,6 +4,14 @@ const app = express();
 
 const port = process.env.PORT || 5001;
 
+app.get("/getallposts", (req, res) => {
+  res.json({
+    status: 200,
+    postTilte: "Hello there",
+    postMessage: "My name is Mansor",
+  });
+});
+
 app.listen(port, () => {
   console.log(`The server is running on port: ${port}`);
 });
