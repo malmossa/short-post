236fe30a830216710f114b694410d48a20578ba0
Mansor <malmossa@gmail.com>
2022-04-21 16:20:01 -0700

    Completed

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 const port = process.env.PORT || 5001;
 
@@ -19,6 +24,36 @@ app.get("/api/posts", (req, res) => {
       },
     },
   });
+});
+
+// Get one post
+app.get("/api/posts/:id", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      postTile: "Hi",
+      postMessage: "Hello there!!",
+    },
+  });
+});
+
+// Create a post
+app.post("/api/posts", (req, res) => {
+  console.log(req.body);
+  res.send("posr was created.");
+});
+
+// Update a post
+app.put("/api/posts/:id", (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+  res.send("post was updated.");
+});
+
+// Delete a post
+app.delete("/api/posts/:id", (req, res) => {
+  console.log(req.params.id);
+  res.send("Post was deleted");
 });
 
 app.listen(port, () => {
