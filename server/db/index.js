49ab993ea721b6805cebd93e7dd38cbc9fe89c49
Mansor Almossa <malmossa@gmail.com>
2022-04-24 14:59:58 -0700

    fixed the database connection

--- server/db/index.js
+++ server/db/index.js
@@ -1,17 +1,5 @@
 const { Pool } = require("pg");
 
-// PGUSER = postgres;
-// PGPASSWORD = Dammam_1973;
-// PGHOST = localhost;
-// PGDATABASE = shortpost;
-// PGPORT = 5433;
-
-const pool = new Pool({
-  user: "postgres",
-  password: "Dammam_1973",
-  host: "localhost",
-  database: "shortpost",
-  port: 5433,
-});
+const pool = new Pool();
 
 module.exports = pool;
