2cd0363ef9690b48974a7c50697ab7cea30f930a
Mansor Almossa <malmossa@gmail.com>
2022-04-29 22:10:52 -0700

    Still working on the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./routes/Home";
+import PostDetailPage from "./routes/PostDetailPage";
+import UpdatePage from "./routes/UpdatePage";
 
 const App = () => {
-  return <div>This is the App component</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/posts/:id" component={PostDetailPage} />
+        <Route exact path="/posts/:id/update" component={UpdatePage} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
